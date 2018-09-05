5d4b0cedb9be8ff5b1bb0317fffda29cff4c4579
Matija Kregar <matija.kregar@gmail.com>
2018-09-05 15:01:13 +0200

    Add a new target for the admin app.

--- Podfile
+++ Podfile
@@ -1,7 +1,12 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '11.0'
 
+use_frameworks!
+
 target 'Vallet' do
-    use_frameworks!
+    pod 'web3swift', '~> 1.1.5'
+end
+
+target 'ValletAdmin' do
     pod 'web3swift', '~> 1.1.5'
 end
---
+++ ValletAdmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ValletAdmin
+//
+//  Created by Matija Kregar on 05/09/2018.
+//  Copyright © 2018 Matija Kregar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
