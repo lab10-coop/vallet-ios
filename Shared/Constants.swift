44b871e1d7f1a80bbd803b626b0b332b377c94ba
Matija Kregar <matija.kregar@gmail.com>
2018-09-05 15:36:05 +0200

    Add the Constants struct with some blockchain related constants.

---
+++ Shared/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Vallet
+//
+//  Created by Matija Kregar on 05/09/2018.
+//  Copyright © 2018 Matija Kregar. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+	struct BlockChain {
+
+		static let faucetServerAddress = "http://faucet.t1.artis.lab10.io/"
+		static let nodeAddress = "http://rpc.t1.artis.lab10.io:10204/"
+		static let tokenFactoryContractAddress = "0x0fd1fa8112f205d5c2754ece65eb43deb3592214"
+
+		struct Event {
+
+			static let tokenCreated = "TokenCreated"
+			
+		}
+	}
+
+}
