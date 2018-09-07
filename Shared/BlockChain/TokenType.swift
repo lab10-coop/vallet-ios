07b0266932fb32247578ab341103c8cab164a1fd
Matija Kregar <matija.kregar@gmail.com>
2018-09-07 13:02:06 +0200

    Add the TokenType enum.

---
+++ Shared/BlockChain/TokenType.swift
@@ -0,0 +1,16 @@
+//
+//  TokenType.swift
+//  Vallet
+//
+//  Created by Matija Kregar on 06/09/2018.
+//  Copyright © 2018 Matija Kregar. All rights reserved.
+//
+
+import Foundation
+
+enum TokenType: String {
+
+	case eur = "V-EUR"
+	case voucher = "Voucher"
+	
+}
