c49e561feb9a8b7be6400c7516654d56f2c050aa
Matija Kregar <matija.kregar@gmail.com>
2018-09-19 12:55:57 +0200

    Add an admin history table view cell.

---
+++ ValletAdmin/UI/Cell/AdminHistoryEventTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  AdminHistoryEventTableViewCell.swift
+//  ValletAdmin
+//
+//  Created by Matija Kregar on 19/09/2018.
+//  Copyright © 2018 Matija Kregar. All rights reserved.
+//
+
+import UIKit
+
+class AdminHistoryEventTableViewCell: UITableViewCell, NibBackedTableViewCell {
+
+	@IBOutlet var descriptionLabel: UILabel!
+	@IBOutlet var valueLabel: UILabel!
+
+	var event: ValueEvent? {
+		didSet {
+			guard let event = event
+				else {
+					prepareForReuse()
+					return
+			}
+			descriptionLabel.text = "\(event.type.description) \(event.clientAddress)"
+			valueLabel.text = event.type == ValueEventType.issue.rawValue ? "- \(event.value)" : "+ \(event.value)"
+		}
+	}
+
+	override func prepareForReuse() {
+		descriptionLabel.text = ""
+		valueLabel.text = ""
+	}
+
+}
+
+
